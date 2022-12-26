61a979d025b089aa650433b761d1a1c9c3d61a09
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2022-12-26 14:53:56 +0700

    Update and optimize code

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class userInput {
+public class UserInput {
     public static String rawInput() {
         while (true) {
             Scanner sc = new Scanner(System.in);
