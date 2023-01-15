6f40897f232a3fb3b613057a9495f49c5eb09480
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2023-01-15 15:32:50 +0700

    Debug code

--- src/bonusFeatures/Cart.java
+++ src/bonusFeatures/Cart.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class Cart {
     private Customer customer = new Customer();
 
+    // Constructor
     public Cart(Customer customer) throws IOException {
         this.customer = customer;
     }
